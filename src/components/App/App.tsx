bf7d86a6343c6cc9b4c7d2f881ed41340b3bd3b7
saifulss <saifulss85@gmail.com>
2019-09-11 22:08:37 +0800

    Tweak sonar config again

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  return <div className="App">booyah travis</div>;
+  return <div className="App">booyah! travis</div>;
 };
 
 export default App;
