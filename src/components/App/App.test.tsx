935501d9882e2ea0e48f4a89bea45f6b35ff63e7
saifulss <saifulss85@gmail.com>
2019-09-25 21:24:31 +0800

    Externalize repeated import to setupTests.ts

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import App from './App';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 
 it('renders without crashing', () => {
   const { getByText } = render(<App />);
